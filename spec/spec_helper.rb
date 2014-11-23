28f601dc4d8b9a46d2b534524a8460e83b689b77
Rahul Horé <hore.rahul@gmail.com>
2014-11-22 21:05:32 -0500

    Removes unneeded spec config options
    - Removes `config.treat_symbols_as_metadata_keys_with_true_values = true`. Apparently, this is now deprecated and set to true by default. There is no longer a false option.
    
    - Removes `config.enable_any_instance_mocks = true`. I get a NoMethodError leaving this in. Not sure what it does, so I'm taking it out for now.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,15 +3,13 @@ require 'volt/spec/setup'
 Volt.spec_setup
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
+  # Run specs in random order to surface order dependencies.
+  # If you find an order dependency and want to debug it,
+  # you can fix the order by providing the seed,
+  # which is printed after each run.
   #     --seed 1234
   config.order = 'random'
-
-  config.enable_any_instance_mocks = true
 end
