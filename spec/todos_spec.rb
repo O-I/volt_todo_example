6beb866163dd830b379d0ae1717c6c4346babd24
Rahul Horé <hore.rahul@gmail.com>
2014-11-25 17:31:56 -0500

    Begins testing todo functionality

--- spec/todos_spec.rb
+++ spec/todos_spec.rb
@@ -1,20 +1,31 @@
 if ENV['BROWSER']
   require 'spec_helper'
 
-  describe 'Todos app navigation', type: :feature do
-    it 'loads the home page as the root' do
-      visit '/'
-      expect(page).to have_content('Home')
+  describe 'Todos app' do
+
+    describe 'Navigation', type: :feature do
+      it 'loads the home page as the root' do
+        visit '/'
+        expect(page).to have_content('Home')
+      end
+
+      it 'has an about page' do
+        visit '/about'
+        expect(page).to have_content('About')
+      end
+
+      it 'has a todos page' do
+        visit '/todos'
+        expect(page).to have_content('Todo List')
+      end
     end
 
-    it 'has an about page', type: :feature do
-      visit '/about'
-      expect(page).to have_content('About')
-    end
-
-    it 'has a todos page', type: :feature do
-      visit '/todos'
-      expect(page).to have_content('Todo List')
+    describe 'Functionality', type: :feature do    
+      it 'creates a new todo' do
+        visit '/todos'
+        fill_in 'Add a new todo', with: 'Foobar the baz with the qux'
+        page.execute_script("$('form').submit()")
+      end
     end
   end
 end
